3b6e6b88980b6efad841d42f36fe2b2479c1f816
sinyagina <Sinyagina060897@mail.ru>
2016-05-24 19:17:13 +0600

    uravn

---
+++ uravn.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+void resh(double a, double b, double c)
+{
+	double d, x1, x2;
+	d = b * b - 4 * a * c;
+	if (d < 0)
+	{
+		printf("net kornei");
+	}
+	else if (d == 0)
+	{
+		x1 = -b / (2 * a);
+		printf("koren: %lf", x1);
+	}
+	else if (d > 0)
+	{
+		x1 = (-b + sqrt(d)) / (2 * a);
+		x2 = (-b - sqrt(d)) / (2 * a);
+		printf("koren: %lf %lf", x1, x2);
+	}
+}
