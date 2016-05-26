afce07467831d41e9971d5481420e74c377d5f8f
sinyagina <Sinyagina060897@mail.ru>
2016-05-26 18:51:39 +0600

    vozvrashaem resultat

--- src/uravn.c
+++ src/uravn.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <math.h>
 
-void resh(double a, double b, double c, double *d, double *x1, double *x2)
+int resh(double a, double b, double c, double *d, double *x1, double *x2)
 {
 	d = b * b - 4 * a * c;
 	if (d < 0)
 	{
 		printf("net kornei");
+		return 0;
 	}
 	else if (d == 0)
 	{
 		x1 = -b / (2 * a);
 		printf("koren: %lf", x1);
+		return 1;
 	}
 	else if (d > 0)
 	{
 		x1 = (-b + sqrt(d)) / (2 * a);
 		x2 = (-b - sqrt(d)) / (2 * a);
 		printf("koren: %lf %lf", x1, x2);
+		return 2;
 	}
 }
