d1d5e8938843d8a7cfce9981ff9cdbe7ea38b994
sinyagina <Sinyagina060897@mail.ru>
2016-05-26 19:19:47 +0600

    test

--- Makefile
+++ Makefile
@@ -3,11 +3,20 @@
 bin/reshuravn: prepare obj/reshuravn.o obj/uravn.o
 	gcc -o bin/reshuravn obj/reshuravn.o obj/uravn.o -lm
 
+bin/test: prepare obj/uravn.o obj/ctest.o obj/test.o
+	gcc -o bin/test obj/uravn.o obj/ctest.o obj/test.o -lm
+
 obj/reshuravn.o: src/reshuravn.c
 	gcc -c src/reshuravn.c -o obj/reshuravn.o
 
 obj/uravn.o: src/uravn.c
 	gcc -c src/uravn.c -o obj/uravn.o
+
+obj/ctest.o: test/ctest.c
+	gcc -c test/ctest.c -o obj/ctest.o -Ithirdparty
+
+obj/test.o: test/test.c
+	gcc -c test/test.c -o obj/test.o -Ithirdparty -Isrc
 
 prepare: bin obj
 
--- test/test.c
+++ test/test.c
@@ -1,57 +1,14 @@
 #include <ctest.h>
-#include <quad_equ.h>
+#include <uravn.h>
 
 CTEST(quad_equ_suite, zeroes)
 {
-    const double a = 0;
-    const double b = 0;
-    const double c = 0;
+	double d, x1, x2;
+    const double a = 0, b = 0, c = 0;
 
-    const Coef equ = quadEqu(a, b, c);
+    const int resultat = resh(a, b, c, &d, &x1, &x2);
     
     const double expected_d = 0;
 
-    ASSERT_EQUAL(expected_d, equ.d);
-}
-
-CTEST(quad_equ_suite, negative_discriminant)
-{
-    const double a = 4;
-    const double b = 0;
-    const double c = 3;
-
-    const Coef equ = quadEqu(a, b, c);
-
-    const double expected_d = -48;
-
-    ASSERT_DBL_NEAR(expected_d, equ.d);
-}
-
-CTEST(quad_equ_suite, one_root)
-{
-    const double a = 1;
-    const double b = -2;
-    const double c = 1;
-
-    const Coef equ = quadEqu(a, b, c);
-
-    const Coef expectedCoef = {0, 1};
-
-    ASSERT_DBL_NEAR(expectedCoef.d, equ.d);
-    ASSERT_DBL_NEAR(expectedCoef.x1, equ.x1);
-}
-
-CTEST(quad_equ_suite, two_roots)
-{
-    const double a = 2;
-    const double b = -3;
-    const double c = 1;
-
-    const Coef equ = quadEqu(a, b, c);
-
-    const Coef expectedCoef = {1, 1, 0.5};
-
-    ASSERT_DBL_NEAR(expectedCoef.d, equ.d);
-    ASSERT_DBL_NEAR(expectedCoef.x1, equ.x1);
-    ASSERT_DBL_NEAR(expectedCoef.x2, equ.x2);
+    ASSERT_EQUAL(expected_d, d);
 }
